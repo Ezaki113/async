d88c4e04fa8fa60b8e8ba00f2e64e4eaf6a07d70
Andrey Kostylev <kostylevandrey113@gmail.com>
2017-02-23 22:46:09 +0300

    ya hz rabotaet

--- test.php
+++ test.php
@@ -1,15 +1,19 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+
+$sockets = stream_socket_pair(AF_UNIX, SOCK_STREAM, STREAM_IPPROTO_IP);
+list($read, $write) = $sockets;
 
 $loop = uv_loop_new();
-$handler = uv_pipe_init($loop, false);
-uv_pipe_open($handler, (int)STDIN);
 
-
-uv_read_start($handler, function ($socket, $nread, $buffer) {
-    var_dump($nread, $buffer);
+$readHandler = uv_pipe_init($loop, false);
+uv_pipe_open($readHandler, (int) $read);
+uv_read_start($readHandler, function ($a, $b, $c) {
+    var_dump($a, $b, $c);
 });
 
+$writeHandler = uv_pipe_init($loop, false);
+uv_pipe_open($writeHandler, (int) $write);
 
-uv_run($loop, \UV::RUN_DEFAULT);
-uv_close($handler);
+uv_write($writeHandler, 'hello world');
+
+uv_run($loop);
